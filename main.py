8264da71574dcac01b760a1f295e25af29d68896
Ashwin sawkare <asawakare@gammastack.com>
2021-12-26 11:13:30 +0530

    post request, and  dynamic url

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+
+
 
 app = FastAPI()
 
+
+class Item(BaseModel):
+    name : str
+    
+    
+    
+@app.post("/item")
+def created_blog(request:Item):
+    
+    return {"data" : f"blog created as name {request.name}"}
+
 @app.get("/")
-def index():
-    return {"data":{"name" : "sharthak"}}
+def index(limit=10, published : bool=True, sort:Optional[str]=None):
+    if published :
+        return {"data":{f"sharthak , {limit}"}}
+    else:
+        return {"data":"None"}
 
 @app.get("/about")
 def about():
     return  {"data": "about page"}
+
+@app.get("/blog/{id}")
+def show(id: int):
+    return{"data":id}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=9000)
