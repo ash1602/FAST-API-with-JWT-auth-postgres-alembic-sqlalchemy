b1cb5e5587daf850473d1c7d81542fe0dd5614d2
Ashwin sawkare <asawakare@gammastack.com>
2021-12-27 14:48:46 +0530

    small correction on put request

--- blog/main.py
+++ blog/main.py
@@ -35,7 +35,7 @@ def update(id: int, request:schemas.Blog, db:Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"blog with id {id} not found")
-    blog.update(request)
+    blog.update(request.dict())
     db.commit()
     return "updated"
 
